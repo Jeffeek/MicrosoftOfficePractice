a94c9600234947c52c42acda20048fa22c1eaa36
Jeffeek <mishamine26@gmail.com>
2020-04-25 22:37:27 +0300

    Оптимизация

---
+++ Excel.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace MicrosoftOfficePractice
+{
+    public class Excel
+    {
+        public string Path { get; private set; }
+        _Application excel = new _Excel.Application();
+        Workbook WorkBook;
+        Worksheet WorkSheet;
+        public Excel(string name, int Sheet)
+        {
+            Path = $"{Directory.GetCurrentDirectory()}\\{name}.xlsx";
+            if (!File.Exists(Path))
+            {
+                var workbook = excel.Workbooks.Add();
+                workbook.SaveAs(Path);
+            }
+            WorkBook = excel.Workbooks.Open(Path);
+            WorkSheet = WorkBook.Worksheets[Sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i++; j++;
+
+            if (WorkSheet.Cells[i, j] != null)
+                return WorkSheet.Cells[i, j].Value2;
+            else
+                return "";
+        }
+
+        public void WriteToCell(int i, int j, string text)
+        {
+            i++; j++;
+            WorkSheet.Cells[i, j].Value2 = text;
+        }
+
+        public void Save()
+        {
+            WorkBook.Save();
+        }
+
+        public void SaveAs(string name)
+        {
+            WorkBook.SaveAs($"{Directory.GetCurrentDirectory()}\\{name}.xlsx");
+        }
+
+        public void CloseWorkBook()
+        {
+            var ExcelProc = Process.GetProcessesByName("Excel");
+            foreach (var process in ExcelProc)
+            {
+                process.Kill();
+            }
+        }
+
+        public void CreateNewFile()
+        {
+            WorkBook = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+            this.WorkSheet = WorkBook.Worksheets[1];
+        }
+
+        public void CreateNewWorksheet()
+        {
+            Worksheet sheet = WorkBook.Worksheets.Add(After: WorkSheet);
+        }
+
+        public void SelectWorksheet(int numberOf)
+        {
+            this.WorkSheet = WorkBook.Worksheets[numberOf];
+        }
+
+        public void DeleteWorksheet(int numberOf)
+        {
+            WorkBook.Worksheets[numberOf].Delete();
+        }
+
+        public void ProtectSheet()
+        {
+            WorkSheet.Protect();
+        }
+
+        public void ProtectSheet(string Password)
+        {
+            WorkSheet.Protect(Password);
+        }
+
+        public void UnprotectSheet()
+        {
+            WorkSheet.Unprotect();
+        }
+
+        public void UnprotectSheet(string Password)
+        {
+            WorkSheet.Unprotect(Password);
+        }
+
+        public string[,] ReadRange(int startI, int startJ, int finishI, int finishJ)
+        {
+            Range range = (Range)WorkSheet.Range[WorkSheet.Cells[startI, startJ], WorkSheet.Cells[finishI, finishJ]];
+            var temp = range.Value2;
+            string[,] ret = new string[finishI - startI + 1, finishJ - startJ + 1];
+            for (int i = 1; i <= finishI - startI; i++)
+            {
+                for (int j = 1; j <= finishJ - startJ; j++)
+                {
+                    if (temp[i, j] == null)
+                    {
+                        ret[i - 1, j - 1] = "";
+                    }
+                    else
+                    {
+                        ret[i - 1, j - 1] = temp[i, j].ToString();
+                    }
+                }
+            }
+            return ret;
+        }
+
+        public void WriteRange(int startI, int startJ, int finishI, int finishJ, string[,] writeArr)
+        {
+            int rowsArr = 0;
+            int ColumnArr = 0;
+            for (int i = startI; i <= finishI; i++)
+            {
+                for (int j = startJ; j <= finishJ; j++)
+                {
+                    WorkSheet.Cells[i, j] = writeArr[rowsArr,ColumnArr];
+                    ColumnArr++;
+                }
+                rowsArr++;
+                ColumnArr = 0;
+            }
+            Save();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelDataReader;
+
+
+namespace MicrosoftOfficePractice
+{
+    public partial class Form1 : Form
+    {
+        Word word;
+        Excel excel;
+        public Form1()
+        {
+            InitializeComponent();
+            //excel = new Excel("HUI", 1);
+            //word = new Word("1");
+            //word.FindAndReplace("{раз}", "228");
+            //word.FindAndReplace("{2раз}", "1337");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            GC.GetTotalMemory(false);
+            if (word != null || excel != null)
+            {
+                if (word != null)
+                {
+                    word.Close();
+                    word = null;
+                }
+                if (excel != null)
+                {
+                    excel.CloseWorkBook();
+                    excel = null;
+                }
+                GC.Collect();
+                GC.GetTotalMemory(false);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var stream = File.Open($"{Directory.GetCurrentDirectory()}\\HUI.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+                    dataGridView1.DataSource = result.Tables[0];
+                }
+            }
+        }
+    }
+}
